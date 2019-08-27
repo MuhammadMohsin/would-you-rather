143360384cb077988e99f3f1ff68657944fe9f11
MuhammadMohsin <mmohsinkhalid@outlook.com>
2019-08-27 14:31:23 +0500

    Implement UI for login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/login.css';
+import TitleImg from '../assets/would-u-r.png';
 
-function App() {
-  return (
-    <div className="login-container">
-      <div></div>
-    </div>
-  );
+class Login extends Component {
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+  }
+  render() {
+    return (
+      <div className="login-container">
+        <img src={TitleImg} alt="title-img" className="title-img" />
+        <h1>Sign in</h1>
+        <p>Please sign in to continue</p>
+        <select className="login-select" onChange={this.handleChange}>
+          <option value="a">a</option>
+          <option value="b">b</option>
+          <option value="c">c</option>
+        </select>
+        <button className="signin-btn">Sign in</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Login;
