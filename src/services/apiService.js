787f5b719d49ef12e15fa53e1989c946a8dea95c
MuhammadMohsin <mmohsinkhalid@outlook.com>
2019-08-27 14:33:00 +0500

    Add api service

---
+++ src/services/apiService.js
@@ -0,0 +1,23 @@
+import {
+    _getUsers,
+    _getQuestions,
+    _saveQuestionAnswer,
+    _saveQuestion,
+} from '../utils/_DATA';
+
+export async function getInitialData() {
+    const users = await _getUsers();
+    const questions = await _getQuestions();
+    return {
+        users,
+        questions
+    }
+}
+
+export function saveQuestionAnswer(info) {
+    return _saveQuestionAnswer(info)
+}
+
+export function saveQuestion(question) {
+    return _saveQuestion(question)
+}
